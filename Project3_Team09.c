8c5c5070538d684e2021930f2d51d1ff88e64052
Jordan Loeser <jordan@Jordans-MacBook-Pro.local>
2017-02-22 21:52:23 -0500

    Initial Commit

---
+++ Project3_Team09.c
@@ -0,0 +1,36 @@
+/* Project 3 Control Code
+ * File:    Project3_Team07.c
+ * Date:    22 January 2017
+ * By:      Klaire Fosnaugh
+ *          kfosnau
+ *          Bailey Hayes
+ *          hayes121
+ *          Jordan Loeser
+ *          jloeser
+ *          Michael Porter
+ *          porte109
+ * Section: 1
+ * Team:    9
+ *
+ * ELECTRONIC SIGNATURE
+ * Klaire Fosnaugh
+ * Bailey Hayes
+ * Jordan Loeser
+ * Michael Porter
+ *
+ * The electronic signatures above indicate that the program
+ * submitted for evaluation is the combined effort of all
+ * team members and that each member of the team was an
+ * equal participant in its creation. In addition, each
+ * member of the team has a general understanding of
+ * all aspects of the program development and execution.
+ *
+ * Commands Autonomous Lunar Vehicle (ALV).
+ */
+
+task main() {
+    /* Get an input, play a sound, and wait 1 second */ int input;
+    input = SensorValue[1];
+        PlaySound(soundBeepBeep);
+        Sleep(1000);
+}
