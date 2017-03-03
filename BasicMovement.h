dc8f9fb12d47cbb08854c8a4acc9438d38932bb9
Jordan Loeser <jordan@Jordans-MacBook-Pro.local>
2017-03-03 17:02:29 -0500

    Prep for Task 2
    Added: move compass functions, turn right function, and moveToPosition
    functions (NEED TO BE TESTED). Created struct to store ALV current
    position.

--- BasicMovement.h
+++ BasicMovement.h
@@ -4,6 +4,15 @@
 #define circumference (diameter * PI)
 
 /*
+Struct for robot's current position.
+*/
+typedef struct Position_struct{
+    int x;
+    int y;
+    int orientation;
+} Position;
+
+/*
 Move forwards a set distance in mm.
 */
 void moveDistanceMm(int distanceMm, int power) {
@@ -37,7 +46,7 @@ void moveDistanceCm(int distanceCm, int power) {
 /*
 Rotate a specific amount of degrees.
 */
-void turnLeftDeg(int degrees, int power) {
+int turnLeftDeg(int degrees, int power) {
 
     // Initialize Variables
     nMotorEncoder[motorA] = 0;
@@ -62,5 +71,146 @@ void turnLeftDeg(int degrees, int power) {
     motor[motorA] = 0;
     motor[motorB] = 0;
 
-    return;
+    return -1 * degrees;
+}
+int turnRightDeg(int degrees, int power) {
+
+    // Initialize Variables
+    nMotorEncoder[motorA] = 0;
+    nMotorEncoder[motorB] = 0;
+
+    // Determine Tickgoal
+    float ticksPerDeg = 1.758;
+    int tickGoal = (ticksPerDeg * degrees);
+
+    // Run Motors
+    motor[motorB] = -1 * power;
+    motor[motorA] = power;
+
+    // Turn
+    while(nMotorEncoder[motorB] > -1 * tickGoal || nMotorEncoder[motorA] < tickGoal) {
+    	if(nMotorEncoder[motorB] < -1 * tickGoal) { motor[motorB] = 0;}
+    	if(nMotorEncoder[motorA] > tickGoal) { motor[motorA] = 0;}
+    }
+
+    // Stop
+    motor[motorA] = 0;
+    motor[motorB] = 0;
+
+    return degrees;
+}
+
+/*
+Face Compass Direction
+*/
+void faceNorth(Position currentPosition, int power){
+
+    int initialOrientation = currentPosition.orientation;
+
+    // Readjust angle to vertical
+    if((initialOrientation % 360) < 180){
+        turnLeftDeg((initialAngle % 360), power);
+    }
+    else {
+        turnRightDeg(360 - (initialAngle % 360), power);
+    }
+
+    currentPosition.orientation = 0;
+
+    return currentPosition; // Degrees
+}
+void faceSouth(Position currentPosition, int power){
+
+    int initialOrientation = currentPosition.orientation;
+
+    // Readjust angle to vertical
+    if((initialOrientation % 360) < 180){
+        turnRightDeg(180 - (initialAngle % 360), power);
+    }
+    else {
+        turnLeftDeg((initialAngle % 360) - 180, power);
+    }
+
+    currentPosition.orientation = 180;
+
+    return currentPosition; // Degrees
+}
+void faceEast(Position currentPosition, int power){
+
+    int initialOrientation = currentPosition.orientation;
+
+    // Readjust angle to vertical
+    if((initialOrientation % 360) < 90){
+        turnRightDeg(90 - (initialAngle % 360), power);
+    }
+    else {
+        turnLeftDeg((initialAngle % 360) - 90, power);
+    }
+
+    currentPosition.orientation = 90;
+
+    return currentPosition; // Degrees
+}
+void faceWest(Position currentPosition, int power){
+
+    int initialOrientation = currentPosition.orientation;
+
+    // Readjust angle to vertical
+    if((initialOrientation % 360) < 270){
+        turnRightDeg(270 - (initialAngle % 360), power);
+    }
+    else {
+        turnLeftDeg((initialAngle % 360) - 270, power);
+    }
+
+    currentPosition.orientation = 270;
+
+    return currentPosition; // Degrees
+}
+
+
+/*
+Move Vertically To a specific Y coord
+*/
+void moveVerticallyTo(int goalYPos, Position currentPosition, int power){
+
+    if(currentPosition.y > goalYPos) { // ALV must move down
+        currentPosition = faceSouth(currentPosition, power);
+        while(currentPosition.y > goalYPos){
+            moveDistanceCm(1, power);
+            currentPosition.y -= 1;
+        }
+    }
+    else { // ALV must move up
+        currentPosition = faceNorth(currentPosition, power);
+        while(currentPosition.y < goalYPos){
+            moveDistanceCm(1, power);
+            currentPosition.y += 1;
+        }
+    }
+
+    return currentPosition;
+}
+
+/*
+Move Vertically To a specific Y coord
+*/
+void moveHorizontallyTo(int goalXPos, Position currentPosition, int power){
+
+    if(currentPosition.x > goalXPos) { // ALV must move west
+        currentPosition = faceWest(currentPosition, power);
+        while(currentPosition.x > goalYPos){
+            moveDistanceCm(1, power);
+            currentPosition.x -= 1;
+        }
+    }
+    else { // ALV must move east
+        currentPosition = faceEast(currentPosition, power);
+        while(currentPosition.x < goalXPos){
+            moveDistanceCm(1, power);
+            currentPosition.x += 1;
+        }
+    }
+
+    return currentPosition;
 }
---
+++ Project3_POC_Task2_Team09.c
@@ -0,0 +1,61 @@
+/* Project 3 POC Task2 Code
+ * File:    Project3_POC_Task2_Team09.c
+ * Date:    22 January 2017
+ * By:      Klaire Fosnaugh
+ *          kfosnau
+ *          Bailey Hayes
+ *          hayes121
+ *          Jordan Loeser
+ *          jloeser
+ *          Michael Porter
+ *          porte109
+ * Section: 1
+ * Team:    9
+ *
+ * ELECTRONIC SIGNATURE
+ * Klaire Fosnaugh
+ * Bailey Hayes
+ * Jordan Loeser
+ * Michael Porter
+ *
+ * The electronic signatures above indicate that the program
+ * submitted for evaluation is the combined effort of all
+ * team members and that each member of the team was an
+ * equal participant in its creation. In addition, each
+ * member of the team has a general understanding of
+ * all aspects of the program development and execution.
+ *
+ * Commands Autonomous Lunar Vehicle (ALV).
+ */
+
+#include "BasicMovement.h"
+
+
+task main() {
+    // Clean Slate
+    nMotorEncoder[motorA] = 0;
+    nMotorEncoder[motorB] = 0;
+
+    // Define Variables
+    Position currentPosition;
+    currentPosition.orientation = 90; // East
+    int goalX = 135; // cm
+    int goalY = 75;
+
+    // Enter Starting Position
+    writeDebugScreen("\nEnter initial X coordinate: ");
+    scanf("%f", &currentPosition.x);
+
+    writeDebugScreen("\nEnter initial Y coordinate: ");
+    scanf("%f", &currentPosition.y);
+
+    // Move to suitable y Position (40)
+    currentPosition = moveVerticallyTo(40, currentPosition, 20);
+
+    // Move to final x Position
+    currentPosition = moveHorizontallyTo(goalX, currentPosition, 20);
+
+    // Move to final y position
+    currentPosition = moveVerticallyTo(goalYPos, currentPosition, 20);
+
+}
